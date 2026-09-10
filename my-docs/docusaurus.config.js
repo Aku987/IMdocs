8409ce1a97dd1cdf7446ab964cafdf12f926b29c
Aku987 <akanksha.sarpal@gmail.com>
2026-09-10 16:18:07 +0530

    update

--- my-docs/docusaurus.config.js
+++ my-docs/docusaurus.config.js
@@ -1,181 +1,158 @@
 // @ts-check
-
-// @type JSDoc annotations allow editor autocompletion and type checking
-// (when paired with @ts-check).
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
-
+ 
 import {themes as prismThemes} from 'prism-react-renderer';
-
+ 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
+ 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'IMdocs',
-  tagline: 'Documentation for IMdocs',
+  tagline: 'Guides, References, and Resources',
   favicon: 'img/favicon.ico',
-
-  // Future flags, see:
-  // https://docusaurus.io/docs/api/docusaurus-config#future
+ 
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true,
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
-
-  // Set the production URL of your site here
-  url: 'https://aku987.github.io',
-
+ 
+  // Set the production url of your site here
+  url: 'https://Aku987.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub Pages deployment, it is often '/<projectName>/'
+  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/IMdocs/',
-
-  // GitHub Pages deployment config
-  organizationName: 'Aku987',
-  projectName: 'docs-as-code-training',
-
+ 
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Aku987', // Usually your GitHub org/user name.
+  projectName: 'IMdocs', // Usually your repo name.
+ 
   onBrokenLinks: 'throw',
-
-  // Even if you don't use internationalization, you can use this field
-  // to set useful metadata like html lang.
+ 
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
+ 
   presets: [
     [
       'classic',
-
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
           sidebarPath: './sidebars.js',
-
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Aku987/docs-as-code-training/tree/main/',
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-
         blog: {
           showReadingTime: true,
-
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
           },
-
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Aku987/docs-as-code-training/tree/main/',
-
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
         },
-
         theme: {
           customCss: './src/css/custom.css',
         },
       }),
     ],
   ],
-
-  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-
-    colorMode: {
-      respectPrefersColorScheme: true,
-    },
-
-    navbar: {
-      title: 'IMdocs',
-
-      logo: {
-        alt: 'IMdocs Logo',
-        src: 'img/logo.svg',
+ 
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        respectPrefersColorScheme: true,
       },
-
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
         },
-        {
-          to: '/blog',
-          label: 'Blog',
-          position: 'left',
-        },
-        {
-          href: 'https://github.com/Aku987/docs-as-code-training',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-
-    footer: {
-      style: 'dark',
-
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href:
-                'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/Aku987/docs-as-code-training',
-            },
-          ],
-        },
-      ],
-
-      copyright: `Copyright © ${new Date().getFullYear()} IMdocs. Built with Docusaurus.`,
-    },
-
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'docs',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'X',
+                href: 'https://x.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
 };
-
+ 
 export default config;
-
