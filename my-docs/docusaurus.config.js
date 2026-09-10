712641e7cccff3d8a02a13abbcf3f8b99bf3fd4e
Aku987 <akanksha.sarpal@gmail.com>
2026-09-10 16:30:09 +0530

    new test

--- my-docs/docusaurus.config.js
+++ my-docs/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
  
   // Set the production url of your site here
-  url: 'https://Aku987.github.io',
+  url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/IMdocs/',
